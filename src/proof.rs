b574732dae81e3a58ca026c1cf3787bc5ab0cea5
Davirain <davirain.yin@gmail.com>
2023-05-31 00:42:55 +0800

    fix

--- src/proof.rs
+++ src/proof.rs
@@ -3,9 +3,8 @@ use crate::cosmos::crypto::PublicKey;
 use crate::error::Error;
 use crate::prelude::*;
 use crate::proof::types::signature_and_data::SignatureAndData;
-// use ed25519_dalek::{PublicKey as Ed25519PublicKey, Signature, Verifier};
-// use secp256k1::{ecdsa, Message, PublicKey as Secp256k1PubliKey, Secp256k1};
-// use libsecp256k1::*;
+use ed25519_dalek::{PublicKey as Ed25519PublicKey, Signature, Verifier};
+use secp256k1::{ecdsa, Message, PublicKey as Secp256k1PubliKey, Secp256k1};
 
 pub mod types;
 
@@ -17,34 +16,32 @@ pub mod types;
 // todo (davirain) ref: https://github.com/cosmos/ibc-go/blob/6f1d8d672705c6e8f5b74a396d883e2834a6b943/modules/light-clients/06-solomachine/types/proof.go#L22
 pub fn verify_signature(
     publik_key: PublicKey,
-    _sign_bytes: Vec<u8>,
-    _signature_and_data: SignatureAndData,
+    sign_bytes: Vec<u8>,
+    signature_and_data: SignatureAndData,
 ) -> Result<(), Error> {
     match publik_key.type_url() {
         ED25519_TYPE_URL => {
-            // let public_key: Ed25519PublicKey = Ed25519PublicKey::from_bytes(&publik_key.to_bytes())
-            //     .map_err(|e| Error::Other(format!("decode Ed25519PublicKey Error({})", e)))?;
+            let public_key: Ed25519PublicKey = Ed25519PublicKey::from_bytes(&publik_key.to_bytes())
+                .map_err(|e| Error::Other(format!("decode Ed25519PublicKey Error({})", e)))?;
 
-            // let signature: Signature = Signature::from_bytes(&signature_and_data.signature)
-            //     .map_err(|e| Error::Other(format!("decode Sinature Failed({})", e)))?;
-            // public_key
-            //     .verify(&sign_bytes, &signature)
-            //     .map_err(|e| Error::Other(format!("ed25519 verify failed: ({})", e)))
-            Ok(())
+            let signature: Signature = Signature::from_bytes(&signature_and_data.signature)
+                .map_err(|e| Error::Other(format!("decode Sinature Failed({})", e)))?;
+            public_key
+                .verify(&sign_bytes, &signature)
+                .map_err(|e| Error::Other(format!("ed25519 verify failed: ({})", e)))
         }
         SECP256K1_TYPE_URL => {
             // ref: https://docs.rs/secp256k1/latest/secp256k1/
-            // let secp = Secp256k1::verification_only();
+            let secp = Secp256k1::verification_only();
 
-            // let public_key = Secp256k1PubliKey::from_slice(&publik_key.to_bytes())
-            //     .map_err(|e| Error::Other(format!("Decode Secp256k1 error({})", e)))?;
-            // let message = Message::parse(&sign_bytes);
-            // // .map_err(|e| Error::Other(format!("Decode Message Error({})", e)))?;
-            // let sig = ecdsa::Signature::from_compact(&signature_and_data.signature)
-            //     .map_err(|e| Error::Other(format!("Decode scdsa Signature failed({})", e)))?;
-            // secp.verify_ecdsa(&message, &sig, &public_key)
-            //     .map_err(|e| Error::Other(format!("verify ecdsa failed({})", e)))
-            Ok(())
+            let public_key = Secp256k1PubliKey::from_slice(&publik_key.to_bytes())
+                .map_err(|e| Error::Other(format!("Decode Secp256k1 error({})", e)))?;
+            let message = Message::from_slice(&sign_bytes)
+                .map_err(|e| Error::Other(format!("Decode Message Error({})", e)))?;
+            let sig = ecdsa::Signature::from_compact(&signature_and_data.signature)
+                .map_err(|e| Error::Other(format!("Decode scdsa Signature failed({})", e)))?;
+            secp.verify_ecdsa(&message, &sig, &public_key)
+                .map_err(|e| Error::Other(format!("verify ecdsa failed({})", e)))
         }
         _ => Err(Error::Other("No Support Crypto type".into())),
     }
